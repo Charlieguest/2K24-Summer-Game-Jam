8cad960c395e91a8a2ddca54974662f15e9571b3
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2024-07-03 00:44:14 +0000

    Small change to allow for null selection

--- Assets/Scripts/UI/CharacterSelectionController.cs
+++ Assets/Scripts/UI/CharacterSelectionController.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class CharacterSelectionController : MonoBehaviour
 {
 	[SerializeField] private bool m_BothSelected;
+	[SerializeField] private int m_Selection;
+
+	public void Start()
+	{
+		m_Selection = 0;
+		Debug.Log(m_Selection);
+	}
 
 	public void Select(InputAction.CallbackContext context)
 	{
@@ -12,13 +19,28 @@ public class CharacterSelectionController : MonoBehaviour
 
 		if (selectionInput > 0.0f)
 		{
-			Debug.Log("Posetive");
+			if(m_Selection == -1)
+			{
+				m_Selection = 0;
+			}
+			else
+			{
+				m_Selection = 1;
+			}
+			Debug.Log(m_Selection);
 		}
 		else if(selectionInput < 0.0f)
 		{
-			Debug.Log("Oder Negative");
+			if (m_Selection == 1)
+			{
+				m_Selection = 0;
+			}
+			else
+			{
+				m_Selection = -1;
+			}
+			Debug.Log(m_Selection);
 		}
-
 	}
 
 	public void Confirm(InputAction.CallbackContext context)
