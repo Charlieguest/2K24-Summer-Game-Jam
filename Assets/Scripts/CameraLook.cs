8d548044ac8e9dd5ee2715fa278086fde84b6fe7
TomSLawrence <thomassamuellawrence@gmail.com>
2024-07-05 17:09:28 +0100

    Got camera working. Fix jitter

--- Assets/Scripts/CameraLook.cs
+++ Assets/Scripts/CameraLook.cs
@@ -6,7 +6,18 @@ using UnityEngine.InputSystem;
 public class CameraLook : MonoBehaviour
 {
     private Vector2 mouseInput;
+
+    [SerializeField] private float mouseSensitivity = 20.0f;
+
+    [SerializeField] private GameObject playerBody;
+
+    private float xRotation = 0.0f;
     
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +30,15 @@ public class CameraLook : MonoBehaviour
         
     }
 
-    void OnLook(InputAction.CallbackContext context)
+    public void OnLook(InputAction.CallbackContext context)
     {
-        mouseInput = context.ReadValue<Vector2>();
+        mouseInput = context.ReadValue<Vector2>() / mouseSensitivity;
+
+        xRotation -= mouseInput.y;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.transform.Rotate(Vector3.up * mouseInput.x);
+
     }
 }
