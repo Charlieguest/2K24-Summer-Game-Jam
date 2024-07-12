0b5a521bc20e7790e9a0010ca2beb79d925a85dc
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2024-07-12 00:02:44 +0000

    Further progress on split screen integration

--- Assets/Scripts/Player Selection/CharacterSelector.cs
+++ Assets/Scripts/Player Selection/CharacterSelector.cs
@@ -1,12 +1,24 @@
 using UnityEngine.InputSystem;
 using UnityEngine;
+using System.ComponentModel.Design;
 
 public class CharacterSelector : PlayerInputManager
 {
+	[Header("Player Controller Prefabs")]
+	[Space]
 	public GameObject CleanerPrefab;
 	public GameObject CatPrefab;
 
+	[Header("Game Manager")]
+	[Space]
 	[SerializeField] private GameManager m_GameManager;
+
+	[Header("Player 2 spawn transform")]
+	[Space]
+	[SerializeField] private GameObject m_Player2;
+
+	[SerializeField] private Transform m_Player1SpawnLocation;
+	[SerializeField] private Transform m_Player2SpawnLocation;
 
 	public void Awake()
 	{
@@ -15,12 +27,10 @@ public class CharacterSelector : PlayerInputManager
 
 		if (GameManager.m_Character1Selection == GameManager.CharacterSelection.Cleaner)
 		{
-			Debug.Log("P1 Cleaner");
 			player1 = Instantiate(CleanerPrefab, new Vector3(0.0f, 5.0f, 0.0f), Quaternion.identity);
 		}
-		else if(GameManager.m_Character2Selection == GameManager.CharacterSelection.Cat)
+		else if(GameManager.m_Character1Selection == GameManager.CharacterSelection.Cat)
 		{
-			Debug.Log("P1 Cat");
 			player1 = Instantiate(CatPrefab, new Vector3(0.0f, 5.0f, 0.0f), Quaternion.identity);
 		}
 	}
@@ -30,17 +40,29 @@ public class CharacterSelector : PlayerInputManager
 		if(GameManager.m_Character2Selection == GameManager.CharacterSelection.Cleaner)
 		{
 			playerPrefab = CleanerPrefab;
-			Debug.Log("P2 Cleaner");
 			JoinPlayer(-1, -1, "GamePad", pairWithDevice: Gamepad.all[0]);
+			m_Player2 = GameObject.FindGameObjectWithTag("Player2");
+
+			m_Player2.transform.position = m_Player2SpawnLocation.position;
+			Debug.Log("Pos set");
 		}
 		else if (GameManager.m_Character2Selection == GameManager.CharacterSelection.Cat)
 		{
 			playerPrefab = CatPrefab;
-			Debug.Log("P2 Cat");
 			JoinPlayer(-1, -1, "GamePad", pairWithDevice: Gamepad.all[0]);
-		}
+			m_Player2 = GameObject.FindGameObjectWithTag("Player2");
 
+			m_Player2.transform.position = m_Player2SpawnLocation.position;
+			Debug.Log("Pos set");
+		}
 	}
 
+	public void SetPos()
+	{
+		m_Player2 = GameObject.FindGameObjectWithTag("Player2");
+
+		m_Player2.transform.position = m_Player2SpawnLocation.position;
+		Debug.Log("Pos set");
+	}
 	
 }
