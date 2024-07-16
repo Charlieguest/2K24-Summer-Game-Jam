3c8afa79127d8a6c6e5a359c6ca9e3de9357f36c
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2024-07-16 00:34:09 +0000

    Modified cat controller to work with splitscreen
    had to get rid of one of the the player input components as it was registering as a gamepad

--- Assets/Scripts/Cat/CatCamera.cs
+++ Assets/Scripts/Cat/CatCamera.cs
@@ -7,10 +7,11 @@ public class CatCamera : MonoBehaviour
 {
     [SerializeField] private float m_f_cameraSpeed = 120.0f;
     [SerializeField] private GameObject m_cameraFollowObject;
+	[SerializeField] private GameObject m_cameraSwivelBase;
 
-    private Vector3 m_followPos;
-    [SerializeField] private float m_f_clampAngle = 80.0f;
-    [SerializeField] private float m_f_inputSensitivity = 150.0f;
+	private Vector3 m_followPos;
+    [SerializeField] private float m_f_clampAngle = 60.0f;
+    [SerializeField] private float m_f_inputSensitivity = 20.0f;
     private float m_f_mouseX;
     private float m_f_mouseY;
     private float m_f_rotY = 0.0f;
@@ -18,7 +19,7 @@ public class CatCamera : MonoBehaviour
 
     private void Start()
     {
-        Vector3 rot = transform.localRotation.eulerAngles;
+        Vector3 rot = m_cameraSwivelBase.transform.localRotation.eulerAngles;
         m_f_rotY = rot.y;
         m_f_rotX = rot.x;
         Cursor.lockState = CursorLockMode.Locked;
@@ -27,6 +28,8 @@ public class CatCamera : MonoBehaviour
 
     public void Handle_LookPerformed(InputAction.CallbackContext context)
     {
+		Debug.Log("Happens");
+
         m_f_mouseX = context.ReadValue<Vector2>().x;
         m_f_mouseY = context.ReadValue<Vector2>().y;
 
@@ -36,7 +39,7 @@ public class CatCamera : MonoBehaviour
         m_f_rotX = Mathf.Clamp(m_f_rotX, -m_f_clampAngle, m_f_clampAngle);
 
         Quaternion localRot = Quaternion.Euler(m_f_rotX, m_f_rotY, 0.0f);
-        transform.rotation = localRot;
+		m_cameraSwivelBase.transform.rotation = localRot;
     }
 
     private void LateUpdate()
@@ -44,6 +47,6 @@ public class CatCamera : MonoBehaviour
         Transform target = m_cameraFollowObject.transform;
 
         float step = m_f_cameraSpeed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+		m_cameraSwivelBase.transform.position = Vector3.MoveTowards(transform.position, target.position, step);
     }
 }
