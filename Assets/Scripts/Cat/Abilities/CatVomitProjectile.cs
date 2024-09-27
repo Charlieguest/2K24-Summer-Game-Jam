229107c3fc5214f15730b00dbd994fee0248f7e5
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2024-09-27 17:12:28 +0000

    Got decals spawning at different sizes and rotations

--- Assets/Scripts/Cat/Abilities/CatVomitProjectile.cs
+++ Assets/Scripts/Cat/Abilities/CatVomitProjectile.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Rendering.HighDefinition;
 using UnityEngine;
 
 public class CatVomitProjectile : MonoBehaviour
@@ -18,14 +19,28 @@ public class CatVomitProjectile : MonoBehaviour
 		if (Physics.SphereCast(transform.position, transform.localScale.x / 2, transform.forward, out hit, 1.0f, m_Mask, QueryTriggerInteraction.UseGlobal))
 		{
 			
-			GameObject vomitDecal = Instantiate(m_VomitDecal, hit.point, Quaternion.identity);
+			GameObject vomitDecalContainer = Instantiate(m_VomitDecal, hit.point, Quaternion.identity);
 
 			//Setting the look rotation for the decal to be the negative normal of the hit object
 			//e.g the opposite of the hit object's side's facing direction.
 			Quaternion lookRoation = Quaternion.LookRotation(-hit.normal);
 
 			//This way it's much less likely for the decals appearance to be skewed.
-			vomitDecal.transform.rotation = lookRoation;
+			vomitDecalContainer.transform.rotation = lookRoation;
+
+			//Get child decal
+			GameObject vomitDecal = vomitDecalContainer.transform.GetChild(0).gameObject;
+			
+			//Getting decal component to set decal size
+			DecalProjector decalProjector = vomitDecal.GetComponent<DecalProjector>();
+
+			float decalSize = Random.Range(0.5f, 1.5f);
+			float decalRotation = Random.Range(0.0f, 360.0f);
+
+			decalProjector.size = new Vector3(decalSize, decalSize, 0.05f);
+
+			//Setting roll rotation of decal
+			vomitDecal.transform.localRotation = Quaternion.Euler(0, 0, decalRotation); 
 
 			Destroy(gameObject);
 		}
